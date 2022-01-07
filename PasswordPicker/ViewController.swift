6b60fa948ddc882e8eed9217ceca3d811630fed7
Oleg Kirsanov <olegkirsanov22@gmail.com>
2022-01-07 20:06:42 +0300

    Setup all views layout and changeColorButton logic.

--- PasswordPicker/ViewController.swift
+++ PasswordPicker/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var generateButton: UIButton!
+    @IBOutlet weak var changeColorButton: UIButton!
+    
+    var isBlack: Bool = false {
+        didSet {
+            if isBlack {
+                self.view.backgroundColor = .black
+            } else {
+                self.view.backgroundColor = .white
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews() {
+        infoLabel.font = .systemFont(ofSize: 12)
+        infoLabel.text = "Нажмите, чтобы сгенерировать пароль"
+        
+        generateButton.backgroundColor = .systemBlue
+        generateButton.tintColor = .white
+        generateButton.layer.cornerRadius = 5
+        
+        changeColorButton.backgroundColor = .systemGreen
+        changeColorButton.tintColor = .white
+        changeColorButton.layer.cornerRadius = 5
     }
 
-
+    @IBAction func generateButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func changeColorPressed(_ sender: Any) {
+        isBlack.toggle()
+    }
+    
 }
 
